26cd845af0e3cf4c84bd85254c7438a1dd3a47a7
imennfaidh8@gmail.com <imennfaidh8@gmail.com>
2025-04-24 20:05:14 +0200

    table transaction with search and clear

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from "../../shared/components/header/header.component";
 import {RouterOutlet} from "@angular/router";
 import {SidebarComponent} from "../../shared/components/sidebar/sidebar.component";
+import {ListTransactionComponent} from "../../modules/admin/list-transaction/list-transaction.component";
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-    imports: [
-        HeaderComponent,
-        RouterOutlet,
-        SidebarComponent
-    ],
+  imports: [
+    HeaderComponent,
+    RouterOutlet,
+    SidebarComponent,
+    ListTransactionComponent
+  ],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.css'
 })
--- src/app/modules/admin/list-transaction/list-transaction.component.ts
+++ src/app/modules/admin/list-transaction/list-transaction.component.ts
@@ -1,31 +1,47 @@
-import {Component, OnInit} from '@angular/core';
-import {TransactionService} from "../../../core/services/transaction.service";
-import {CommonModule} from "@angular/common";
-import {TableModule} from "primeng/table";
-import {Transaction} from "../../../shared/models/transaction";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TransactionService } from "../../../core/services/transaction.service";
+import { CommonModule } from "@angular/common";
+import { Table, TableModule } from "primeng/table";
+import { Transaction } from "../../../shared/models/transaction";
+import { IconField } from "primeng/iconfield";
+import { InputIcon } from "primeng/inputicon";
+import { FormsModule } from "@angular/forms";
+import { Button } from "primeng/button";
+import { InputText } from "primeng/inputtext";
 
 @Component({
   selector: 'app-list-transaction',
   standalone: true,
-  imports: [TableModule, CommonModule],
+  imports: [TableModule, CommonModule, IconField, InputIcon, FormsModule, Button, InputText],
   templateUrl: './list-transaction.component.html',
-  styleUrl: './list-transaction.component.css'
+  styleUrls: ['./list-transaction.component.css']
 })
-export class ListTransactionComponent implements OnInit{
+export class ListTransactionComponent implements OnInit {
+  transactions: Transaction[] = [];
+  selectedTransaction!: Transaction;
+  searchValue: string = '';  // Valeur de la recherche
+  @ViewChild('dt') table!: Table; // Référence au tableau PrimeNG
 
-  transactions : Transaction[] =[];
-  constructor(private transactionService : TransactionService,) {
-  }
-  ngOnInit() {
+  constructor(private transactionService: TransactionService) {}
+
+  ngOnInit(): void {
     this.getAllTransaction();
-
   }
 
-  getAllTransaction(){
-    this.transactionService.getAllTransaction().subscribe((data)=> {
+  getAllTransaction(): void {
+    this.transactionService.getAllTransaction().subscribe((data) => {
       this.transactions = data;
     });
   }
 
+  // Cette méthode peut être utilisée si tu veux effectuer une logique supplémentaire lors de la recherche
+  onSearch() {
+    // Le tableau sera automatiquement filtré grâce au globalFilter
+  }
 
+  // Réinitialiser la valeur de recherche et les filtres du tableau
+  clear(dt: Table) {
+    this.searchValue = '';  // Réinitialiser la valeur de recherche
+    dt.clear();  // Réinitialise tous les filtres
+  }
 }
